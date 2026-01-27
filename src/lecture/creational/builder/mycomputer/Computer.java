2a32c82cf62f86189cd7c288d757e1b2eb6641b5
tandaoduy <ddtan071205.nvtroi2023@gmail.com>
2026-01-27 13:45:01 +0700

    update

--- src/lecture/creational/builder/mycomputer/Computer.java
+++ src/lecture/creational/builder/mycomputer/Computer.java
@@ -1,4 +1,48 @@
 package lecture.creational.builder.mycomputer;
 
-public class compuyter {
+public class Computer {
+    private String cpu, ram, storage, screen;
+    //Định nghĩa 1 lớp abstract builder(trong trường hợp này ko cần viết abstract)
+    //--> vết luôn bên trong computer
+    private Computer(Builder builder){
+        this.cpu = builder.cpu;
+        this.ram = builder.ram;
+        this.storage = builder.storage;
+        this.screen = builder.screen;
+    }
+
+    @Override
+    public String toString() {
+        return "computer{" +
+                "cpu='" + cpu + '\'' +
+                ", ram='" + ram + '\'' +
+                ", storage='" + storage + '\'' +
+                ", screen='" + screen + '\'' +
+                '}';
+    }
+
+    public  static class Builder{
+        private String cpu, ram, storage, screen;
+        public Builder buildCPU(String cpu){
+            this.cpu = cpu;
+            return this;
+        }
+        public Builder buildRAM(String ram){
+            this.ram = ram;
+            return  this;
+        }
+        public Builder buildStorage(String storage){
+            this.storage = storage;
+            return this;
+        }
+        public Builder buildScreen(String screen){
+            this.screen = screen;
+            return this;
+        }
+
+
+        public Computer builder(){
+            return new Computer(this);
+        }
+    }
 }
--- src/lecture/creational/builder/mycomputer/MainComputer.java
+++ src/lecture/creational/builder/mycomputer/MainComputer.java
@@ -1,4 +1,13 @@
 package lecture.creational.builder.mycomputer;
 
 public class MainComputer {
+    public static void main(String[] args) {
+        Computer computer = new Computer.Builder()
+                .buildCPU("Intel CPU i7")
+                .buildRAM("64 GB ĐR6 5200Hz")
+                .buildStorage("5 TB NVMe")
+                .buildScreen("8K 24 inches LG")
+                .builder();
+        System.out.println(computer.toString());
+    }
 }
